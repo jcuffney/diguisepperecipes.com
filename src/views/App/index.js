2994b42b90f75e197045f45699962e2535cb5fef
Joseph Cuffney <josephcuffney@gmail.com>
2019-04-07 21:33:52 -0700

    relative path for store

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from 'store'
+import store from '../../store'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import HomeView from 'views/HomeView'
